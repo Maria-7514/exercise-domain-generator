0508e635e95aecb592ae13719490b4bdad79853a
Maria-7514 <evangeline73.mjhe@gmail.com>
2021-03-08 20:52:05 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Any string");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".us", ".io", ".go.cr"];
+
+let elementa = Math.floor(Math.random() * pronoun.length);
+let elementb = Math.floor(Math.random() * adj.length);
+let elementc = Math.floor(Math.random() * noun.length);
+let elementd = Math.floor(Math.random() * domain.length);
+
+let cambioa = pronoun[elementa];
+let cambiob = adj[elementb];
+let cambioc = noun[elementc];
+let cambiod = domain[elementd];
+
+let change = document.getElementById("Domain");
+change.innerHTML = cambioa + "" + cambiob + cambioc + "" + cambiod;
